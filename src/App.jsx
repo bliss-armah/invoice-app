ccb5b8066f82fa3541b66cef468db7de0ae2c7bb
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2023-01-17 07:31:14 +0000

    Added darkmode effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 
 function App() {
+  // Darkmode Effect
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
-    <div>
+    <div className={`App ${darkMode ? 'dark' : 'light'}`}>
+
+      {/* Darkmode button */}
+      <button
+        style={{ position: "absolute", top: "5%", left: "5%", zIndex: "200" }}
+        onClick={toggleDarkMode}
+      >
+        Toggle Dark Mode
+      </button>
 
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
