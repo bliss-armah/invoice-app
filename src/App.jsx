2504f66091198abff73d8cbeb58df103fbd9af62
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-01-25 10:56:31 +0000

    logic for validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Nav from "./components/Home/Nav/Nav";
 import Home from "./pages/Home";
 import ConfirmDelete from "./components/confirmDelete/ConfirmDelete";
 import CreateInvoice from "./components/createInvoice/CreateInvoice";
 import Edit from "./components/editInvoiceForm/Edit";
+import Home from "./pages/Home";
+
 
 function App() {
   // Darkmode Effect
@@ -11,18 +13,18 @@ function App() {
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
-
+ 
+   
   return (
     <div className={`App ${darkMode ? 'dark' : 'light'} lg:flex`}>
       <Nav darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       <Home darkMode={darkMode}/>
       {/* <ConfirmDelete darkMode={darkMode} /> */}
-
-      <CreateInvoice darkMode={darkMode} />
+      {/* <CreateInvoice darkMode={darkMode} /> */}
 
       
-      {/* <Edit darkMode = {darkMode}/> */}
-      </div>
+      <Edit darkMode = {darkMode}/>
+    </div>
   );
   
   }
