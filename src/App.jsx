14e4e09dce5e55435c8f640cf27bcdb70cbb0e50
manuel-larbi <emmanuel.larbi@amalitech.com>
2023-01-27 13:44:11 +0000

    resolved conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import { useState,useEffect } from "react";
 import Nav from "./components/Home/Nav/Nav";
-import ConfirmDelete from "./components/confirmDelete/ConfirmDelete";
-import CreateInvoice from "./components/createInvoice/CreateInvoice";
-import Edit from "./components/editInvoiceForm/Edit";
 import Viewinvoice from "./pages/Viewinvoice";
 import Home from "./pages/Home";
 import {Routes,Route} from "react-router-dom"
-
-import Viewinvoice from "./pages/Viewinvoice"
 
 
 function App() {
@@ -22,10 +17,9 @@ function App() {
 
     <div className={`App ${darkMode ? 'dark' : 'light'} lg:flex`}>
       <Nav darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-
       <Routes>
       <Route path="/" exact element={<Home darkMode={darkMode}/>} />
-      <Route path="/viewinvoice/:id"  element={<ViewInvoice/>} />
+      <Route path="/viewinvoice/:id"  element={<Viewinvoice />} />
     </Routes>
       </div>
 
