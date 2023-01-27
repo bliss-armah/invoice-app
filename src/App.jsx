babba8395cb288d27e9ed2cadc69ee74787f1552
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-01-27 09:09:53 +0000

    view logic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState,useEffect } from "react";
 import Nav from "./components/Home/Nav/Nav";
-import ConfirmDelete from "./components/confirmDelete/ConfirmDelete";
-import CreateInvoice from "./components/createInvoice/CreateInvoice";
-import Edit from "./components/editInvoiceForm/Edit";
-import Viewinvoice from "./pages/Viewinvoice";
 import Home from "./pages/Home";
+import ViewInvoice from "./pages/Viewinvoice";
 import {Routes,Route} from "react-router-dom"
 
 import Viewinvoice from "./pages/Viewinvoice"
