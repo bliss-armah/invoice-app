7541de7675e4f747d2ac42fa15642498039308f9
manuel-larbi <emmanuel.larbi@amalitech.com>
2023-02-02 08:57:26 +0000

    resolved conflict

--- src/pages/Viewinvoice.jsx
+++ src/pages/Viewinvoice.jsx
@@ -267,6 +267,17 @@ function Viewinvoice({ darkMode }) {
                   </div>
                 </div>
               </section>
+              {
+              //   loaded
+              //   ? grandTotal
+              //   // <div className={`blue-box ${
+              //   //   darkMode ? "blue-box-dark" : ""
+              //   // }`}>
+              //   //   <div className="grand-total">Grand Total</div>
+              //   //   <div className="amount">{ grandTotal }</div>
+              //   // </div>
+              // : console.log(loaded, grandTotal) 
+              }
 
               <div className={`blue-box ${darkMode ? "blue-box-dark" : ""}`}>
                 <div className="grand-total">Grand Total</div>
