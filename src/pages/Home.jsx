75ebaa7ae07426235c59bf9f6dee6d3478221461
manuel-larbi <emmanuel.larbi@amalitech.com>
2023-02-02 17:16:33 +0000

    resolved Conflict

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,44 +33,34 @@ const Home = ({darkMode}) => {
   return (
     <>
       <div className='p-6 pt-28 md:px-9 space-y-3 font-spartan lg:w-full h-screen  
-        lg:px-36 xl:px-60 2xl:px-[20%] min-[2560px]:px-[30%] overflow-auto'>
+        lg:px-36 xl:pt-12 xl:px-60 2xl:px-[20%] min-[2560px]:px-[30%] overflow-auto'>
         <InvoiceNav invoice={invoice} darkMode={darkMode} checkStatus={checkStatus}/>
 
         <div className='space-y-5'>
-
-           {
-               invoice.length
-                ? invoicefilter.length 
-                  ? invoice.filter(result => invoicefilter.includes(result.status)).map((invoice,key)=>{
-                  return <Card darkMode={darkMode} key={key} invoiceId={invoice.id} 
-                                name={invoice.clientName} dueDate={invoice.paymentDue}
-                                amount={invoice.total} status={invoice.status}
-                          />  
-                }) : 
-                invoice.map((invoice,key)=>{
+          {
+              invoice.length
+              ? invoicefilter.length 
+                ? invoice.filter(result => invoicefilter.includes(result.status)).map((invoice,key)=>{
+                return <Card darkMode={darkMode} key={key} invoiceId={invoice.id} 
+                          name={invoice.clientName} dueDate={invoice.paymentDue}
+                          amount={invoice.total} status={invoice.status}
+                      />  
+              }) : 
+              invoice.map((invoice,key)=>{
                 return (
-
                   <div key={key}>
-
-                <Link to={`/viewinvoice/${invoice.id}`}>
-                <Card darkMode={darkMode} invoiceId={invoice.id} 
-                              name={invoice.clientName} dueDate={invoice.paymentDue}
-                              amount={invoice.total} status={invoice.status}
-                              />  
-                              </Link>
-                            
-                              </div>
-                              )
-              })
-              : <NoContent /> 
-           }
-        {/* {
-          console.log(invoice.length)
-        } */}
+                    <Link to={`/viewinvoice/${invoice.id}`}>
+                      <Card darkMode={darkMode} invoiceId={invoice.id} 
+                        name={invoice.clientName} dueDate={invoice.paymentDue}
+                        amount={invoice.total} status={invoice.status}
+                      />  
+                    </Link>
+                  </div>
+                )
+            })
+            : <NoContent /> 
+          }
         </div>
-        {/* <CreateInvoice darkMode={darkMode} /> */}
-
-        {/* <Viewinvoice /> */}
       </div>
     </>
   )
