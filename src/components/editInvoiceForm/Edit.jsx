f6f79ec83c19a9a654c6f2b25119a58fefa14e74
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-02-03 16:03:07 +0000

    Functionalities made

--- src/components/editInvoiceForm/Edit.jsx
+++ src/components/editInvoiceForm/Edit.jsx
@@ -218,7 +218,7 @@ const Edit = ({ darkMode, goBack, hold}) => {
 
 
   return (
-    <main >
+    <main className="all">
       <form
         className={`sections ${
           darkMode ? "dark-section " : "light-section"
