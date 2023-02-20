be096e0506e02b99167c8c30f5c9284c8e7c8aa1
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-02-20 16:09:34 +0000

    home card border styled

--- src/invoiceSlice/InvoiceSlice.js
+++ src/invoiceSlice/InvoiceSlice.js
@@ -1,91 +1,74 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios";
 
-const url = "https://invoice-api-9l7b.onrender.com/invoice";
+
+  const url = 'https://invoice-api-9l7b.onrender.com/invoice'
 
 
-const initialState = {
-  invoiceData: [],
-  isLoading: true,
-  isDarkMode: false,
-};
+  
+  const initialState = { 
+    invoiceData: [],
+    isLoading: true,
+    isDarkMode: false
+  };
 
-// export const deleteItem = (id) => async (dispatch) => {
-//   try {
-//     await axios.delete(`https://invoice-api-9l7b.onrender.com/invoice/${id}`, {
-//     });
-//     dispatch({ type: "DELETE_ITEM", payload: id });
-//   } catch (error) {
-//     console.error(error.message)
-//     console.error(error);
-//   }
-// };
+  export const deleteItem = createAsyncThunk(
+    'invoice/deleteItem',
+    async (itemId) => {
+      const response = await axios.delete(`https://invoice-api-9l7b.onrender.com/invoice/${itemId}`)
+      return response.data
+    }
+  )
 
-
-
-
-export const getInvoiceItems = createAsyncThunk(
-  "invoice/getInvoiceItems",
+ export const getInvoiceItems = createAsyncThunk(
+  'invoice/getInvoiceItems',
   async () => {
     try {
       const resp = await axios(url);
       return resp.data;
     } catch (error) {
-      return error.message;
+      return error.message
     }
   }
-  );
-  
-  const InvoiceSlice = createSlice({
-    name: "invoice",
-    initialState,
-    reducers: {
-      addToInvoice: (state, action) => {
-        state.invoiceData = action.payload;
-        console.log(invoiceData);
-      },
-      
-      toggleDarkMode: (state) => {
-        state.isDarkMode = !state.isDarkMode;
-      },
-      
-      deleteInvoice: (state, action) => {
-     
-      //   async() => {
-      //     try {
-      //       await axios.delete(`https://invoice-api-9l7b.onrender.com/invoice/${id}`)
-      //       console.log(id)
-      // } catch (error) {
-      //   console.log(error);
-      // }
-      //   }
-      state.invoiceData = state.invoiceData.filter((item) => item.id !== action.payload )
-    }
-  },
-  
-  extraReducers: (builder) => {
-    builder
-    .addCase(getInvoiceItems.pending, (state) => {
-      state.isLoading = true;
-      })
-      .addCase(getInvoiceItems.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.invoiceData = action.payload;
-      })
-      .addCase(getInvoiceItems.rejected, (state, action) => {
-        state.isLoading = false;
-      });
-  },
-});
+);
 
-export const {
-  addInvoice,
-  updateInvoice,
-  isLoading,
-  isDarkMode,
-  invoiceData,
-  addToInvoice,
-  toggleDarkMode,
-  deleteInvoice,
-} = InvoiceSlice.actions;
-export default InvoiceSlice.reducer;
+
+ 
+ const InvoiceSlice = createSlice({
+     name: 'invoice',
+     initialState,
+     reducers: {
+      addToInvoice:(state,action) =>{
+        state.invoiceData = action.payload
+      },
+
+      toggleDarkMode: (state) => {
+      state.isDarkMode = !state.isDarkMode
+        },
+
+        deleteInvoice: (state, action ) => {
+          const updatedItems = state.invoiceData.filter(item => item.id !== action.payload.id)
+          state.invoiceData = updatedItems
+        }
+      
+     },
+     extraReducers: (builder)=> {
+      builder.addCase(getInvoiceItems.pending,(state) => {
+        state.isLoading = true;
+      },).addCase(getInvoiceItems.fulfilled,(state, action) => {
+        state.isLoading = false;
+        state.invoiceData = action.payload;
+      },).addCase(getInvoiceItems.rejected,(state, action) => {
+        console.log(action);
+        state.isLoading = false;
+      },)
+
+      
+    },
+
+   
+
+})
+
+export const {addInvoice, updateInvoice, isLoading, isDarkMode, invoiceData, addToInvoice, toggleDarkMode, deleteInvoice} = InvoiceSlice.actions
+export default InvoiceSlice.reducer
---
+++ src/invoiceSlice/deleteSlice.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const deleteSlice = () => {
+    
+  return (
+    <div>deleteSlice</div>
+  )
+}
